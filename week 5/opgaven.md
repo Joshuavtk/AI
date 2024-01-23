52565097d7334bcea077198fb057d65060ce1b3e
StechWasTaken <41377140+StechWasTaken@users.noreply.github.com>
2024-01-23 13:55:32 +0100

    Finish 5.2

---
+++ week 5/opgaven.md
@@ -0,0 +1,21 @@
+### Opgave 1
+
+
+
+### Opgave 2
+
+a) key => vorige woord(en), value => een lijst van alle mogelijke opvolgende woorden met bijbehorende gewichten
+
+b) Nord => 0.5, Midi => 0.5
+
+c) beter. => 1
+
+d) Zie [start_generator.py](start_generator.py)
+
+e) Aan de hand van het vorige woord of woorden wordt er een gewogen willekeurige keuze gemaakt. Het blijft doorgaan totdat het einde van een zin.
+
+### Opgave 3
+
+
+
+### Opgave 4
--- week 5/start_generator.py
+++ week 5/start_generator.py
@@ -9,9 +9,21 @@ text = f.read()
 # train and print model for n=2
 text_model = markovify.Text(text, state_size=2)
 
+val = text_model.chain.model[('Gare', 'du')]
+print(val)
+
 # generate some sentences
+for _ in range(5):
+    sentence = text_model.make_sentence()
+    print(sentence)
 
 # train and print model for n=3
+text_model = markovify.Text(text, state_size=3)
+
+val = text_model.chain.model[('Japi', 'wist', 'wel')]
+print(val)
 
 # generate sentences n=3
-
+for _ in range(5):
+    sentence = text_model.make_sentence(tries=9999)
+    print(sentence)
